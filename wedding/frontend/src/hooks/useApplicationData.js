0176c6cc31e7b6d49d31806edb3f272ed0c73aff
Johannes Weber <johannes@14oranges.com>
2023-02-08 22:01:43 -0800

    prod-test works

--- wedding/frontend/src/hooks/useApplicationData.js
+++ wedding/frontend/src/hooks/useApplicationData.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 export default function useApplicationData(initialState) {
   
 //  const T_API_SERVER_URL = "http://localhost";
-  const T_API_SERVER_URL = "http://jowe.ddns.net:10001";
+  const T_API_SERVER_URL = "https://jowe.ddns.net:10001";
 
   const T_DEFAULT_EMBED_ID = 'PogK0wZLFiQ'; //default
 
