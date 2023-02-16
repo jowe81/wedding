b8c3555e8905b4ec7a2edd676e4c4b5a020db811
Johannes Weber <johannes@14oranges.com>
2023-02-16 14:11:28 -0800

    more trivia

--- wedding/frontend/src/components/Trivia.js
+++ wedding/frontend/src/components/Trivia.js
@@ -14,15 +14,18 @@ const Trivia = (appData) => {
         <section className="trivia-section">
           The website you are looking at is being served from <b>a computer that Jess and Johannes built together</b>!
         </section>
-        <section>
+        <section className="trivia-section">
+          It runs Ubuntu Server 22.04 and serves in many capacities such as WAN router, NAS (fileserver), DNS/DHCP server, OpenVPN server, home automation server and Docker host.
+        </section>
+        <section className="trivia-section">
           <img src="/trivia-assembling-computer.jpg" />
         </section>
-        <section>
+        <section className="trivia-section">
           The current nominal internet <b>uplink speed is 100 mpbs</b>. The production-optimized assets for this site yield a <b>total payload of 1.4 MB</b>.
           In theory this means that in the best case scenario the server can cater to about 4 new visits per second, or <b>240 new visits per minute</b>.
           Hopefully that will be sufficient!
         </section>
-        <section>
+        <section className="trivia-section">
           <img src="/trivia-server.jpg" />
         </section>
 
